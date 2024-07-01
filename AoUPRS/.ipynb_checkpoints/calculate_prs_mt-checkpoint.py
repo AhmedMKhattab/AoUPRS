08d9562bf6f89009b28abf60ca66b7924055cc27
Ahmed Khattab <akhattab@ims02.scripps.edu>
2024-07-01 10:50:03 -0700

    Update imports

--- AoUPRS/.ipynb_checkpoints/calculate_prs_mt-checkpoint.py
+++ AoUPRS/.ipynb_checkpoints/calculate_prs_mt-checkpoint.py
@@ -51,13 +51,18 @@ def calculate_prs_mt(mt, prs_identifier, pgs_weight_path, output_path, bucket=No
         hail_fp = f'{output_path}/hail/'
         gc_csv_fp = f'{output_path}/score/{prs_identifier}_scores.csv'
         gc_found_csv_fp = f'{output_path}/score/{prs_identifier}_found_in_aou.csv'
-        
-    # Read PRS weight table
-    if bucket:
-        with gcsfs.GCSFileSystem().open(PGS_path, 'rb') as gcs_file:
-            prs_df = pd.read_csv(gcs_file)
-    else:
-        prs_df = pd.read_csv(PGS_path)
+    
+    print("Reading PRS weight table...")
+    try:
+        if bucket:
+            with gcsfs.GCSFileSystem().open(PGS_path, 'rb') as gcs_file:
+                prs_df = pd.read_csv(gcs_file)
+        else:
+            prs_df = pd.read_csv(PGS_path)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"The file {PGS_path} was not found. Please check the file path and try again.")
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
     
     # Ensure the required columns are present
     required_columns = ["variant_id", "weight", "contig", "position", "effect_allele", "noneffect_allele"]
@@ -71,23 +76,28 @@ def calculate_prs_mt(mt, prs_identifier, pgs_weight_path, output_path, bucket=No
     column_types.update({"weight": "tfloat", "position": "tint32"})
     column_types.update({col: "tstr" for col in optional_columns})
     
+    print("Re-importing PRS table with determined column types...")
     # Re-import the PRS table with the determined column types
     prs_table = hl.import_table(PGS_path, types=column_types, delimiter=',')
     prs_table = prs_table.annotate(locus=hl.locus(prs_table.contig, prs_table.position))
     prs_table = prs_table.key_by('locus')
 
+    print("Filtering variants...")
     # Filter variants
     mt_prs = mt.semi_join_rows(prs_table)
     
+    print("Annotating the MatrixTable with the PRS information...")
     # Annotate the MatrixTable with the PRS information and calculate effect allele count
     mt_prs = mt_prs.annotate_rows(prs_info=prs_table[mt_prs.locus])
 
+    print("Calculating effect allele count and multiplying by variant weight...")
     # Calculate effect allele count and multiply by variant weight in a single step
     effect_allele_count_expr = calculate_effect_allele_count(mt_prs)
     mt_prs = mt_prs.annotate_entries(
         effect_allele_count=effect_allele_count_expr,
         weighted_count=effect_allele_count_expr * mt_prs.prs_info['weight'])
 
+    print("Summing the weighted counts per sample and counting the number of variants with weights per sample...")
     # Sum the weighted counts per sample and count the number of variants with weights per sample
     mt_prs = mt_prs.annotate_cols(
         sum_weights=hl.agg.sum(mt_prs.weighted_count),
@@ -95,6 +105,7 @@ def calculate_prs_mt(mt, prs_identifier, pgs_weight_path, output_path, bucket=No
     
     # Optional: Extract and save found variants
     if save_found_variants:
+        print("Extracting and saving found variants...")
         found_variants_table = mt_prs.filter_rows(hl.is_defined(mt_prs.prs_info)).rows()
         found_prs_info_df = found_variants_table.select(found_variants_table.prs_info).to_pandas()
         
@@ -109,9 +120,11 @@ def calculate_prs_mt(mt, prs_identifier, pgs_weight_path, output_path, bucket=No
             found_prs_info_df.to_csv(gc_found_csv_fp, header=True, index=False, sep=',')
         print(f"Found variants saved as: {gc_found_csv_fp}")
 
+    print(f"Writing the PRS scores to a Hail Table at: {hail_fp}")
     # Write the PRS scores to a Hail Table
     mt_prs.key_cols_by().cols().write(hail_fp, overwrite=True)
     
+    print("Exporting the Hail Table to a CSV file...")
     # Export the Hail Table to a CSV file
     saved_mt = hl.read_table(hail_fp)
     if bucket:
--- AoUPRS/.ipynb_checkpoints/calculate_prs_vds-checkpoint.py
+++ AoUPRS/.ipynb_checkpoints/calculate_prs_vds-checkpoint.py
@@ -30,17 +30,17 @@ def calculate_prs_vds(vds, prs_identifier, pgs_weight_path, output_path, bucket=
     print("")
     print("******************************************")
     print("*                                        *")
-    print("*  Initializing, PRS protocol activated! *")
+    print("*       Ahoy, PRS treasures ahead!       *")
     print("*                                        *")
     print("******************************************")
     print("")
-    
+
     print("")
     print("<<<<<<<<<>>>>>>>>")
-    print(f"    {prs_identifier}   ")
+    print(f"   {prs_identifier}   ")
     print("<<<<<<<<<>>>>>>>>")
     print("")
-    
+
     # Construct paths
     if bucket:
         PGS_path = f'{bucket}/{pgs_weight_path}'
@@ -54,28 +54,43 @@ def calculate_prs_vds(vds, prs_identifier, pgs_weight_path, output_path, bucket=
         hail_fp = f'{output_path}/hail/'
         gc_csv_fp = f'{output_path}/score/{prs_identifier}_scores.csv'
         gc_found_csv_fp = f'{output_path}/score/{prs_identifier}_found_in_aou.csv'
+
     
     # Read PRS weight table
-    if bucket:
-        with gcsfs.GCSFileSystem().open(PGS_path, 'rb') as gcs_file:
-            prs_df = pd.read_csv(gcs_file)
-    else:
-        prs_df = pd.read_csv(PGS_path)
-        
+    print("Reading PRS weight table...")
+    try:
+        if bucket:
+            with gcsfs.GCSFileSystem().open(PGS_path, 'rb') as gcs_file:
+                prs_df = pd.read_csv(gcs_file)
+        else:
+            prs_df = pd.read_csv(PGS_path)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"The file {PGS_path} was not found. Please check the file path and try again.")
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
+
     prs_df['end'] = prs_df['position']
     prs_interval_df = prs_df[['contig', 'position', 'end']]
     
-    if bucket:
-        with gcsfs.GCSFileSystem().open(interval_fp, 'w') as gcs_file:
-            prs_interval_df.to_csv(gcs_file, header=False, index=False, sep="\t")
-    else:
-        prs_interval_df.to_csv(interval_fp, header=False, index=False, sep="\t")
+    print(f"Saving intervals...")
+    try:
+        if bucket:
+            with gcsfs.GCSFileSystem().open(interval_fp, 'w') as gcs_file:
+                prs_interval_df.to_csv(gcs_file, header=False, index=False, sep="\t")
+        else:
+            prs_interval_df.to_csv(interval_fp, header=False, index=False, sep="\t")
+    except Exception as e:
+        raise Exception(f"Failed to save intervals to {interval_fp}. Error: {e}")
         
     print(f"Intervals saved as: {interval_fp}")
     
     # Import locus intervals and filter variants
+    print("Importing locus intervals and filtering variants...")
     prs_sites = hl.import_locus_intervals(interval_fp, reference_genome='GRCh38', skip_invalid_intervals=True)
+    
+    print("Filtering intervals in VDS...")
     vds_prs = hl.vds.filter_intervals(vds, prs_sites, keep=True)
+    print("Filtered intervals successfully.")
 
     # Import PRS weights file
     ## Ensure the required columns are present
@@ -91,35 +106,43 @@ def calculate_prs_vds(vds, prs_identifier, pgs_weight_path, output_path, bucket=
     column_types.update({col: "str" for col in optional_columns})
     
     ## Re-import the PRS table with the determined column types
+    print("Re-importing PRS table...")
     prs_table = hl.import_table(PGS_path, types=column_types, delimiter=',')
     prs_table = prs_table.annotate(locus=hl.locus(prs_table.contig, prs_table.position))
     prs_table = prs_table.key_by('locus')
+    print("PRS table re-imported successfully.")
     
     # Annotate the MatrixTable with the PRS information
+    print("Annotating the MatrixTable with PRS information...")
     mt_prs = vds_prs.variant_data.annotate_rows(prs_info=prs_table[vds_prs.variant_data.locus])
     mt_prs = mt_prs.unfilter_entries() 
         
     # Calculate effect allele count and multiply by variant weight in a single step
+    print("Calculating effect allele count...")
     effect_allele_count_expr = calculate_effect_allele_count_na_hom_ref(mt_prs)
     mt_prs = mt_prs.annotate_entries(
         effect_allele_count=effect_allele_count_expr,
         weighted_count=effect_allele_count_expr * mt_prs.prs_info['weight'])
     
     # Sum the weighted counts per sample and count the number of variants with weights per sample
+    print("Summing weighted counts per sample...")
     mt_prs = mt_prs.annotate_cols(
         sum_weights=hl.agg.sum(mt_prs.weighted_count),
         N_variants=hl.agg.count_where(hl.is_defined(mt_prs.weighted_count)))
     
     # Write the PRS scores to a Hail Table
+    print(f"Writing the PRS scores to a Hail Table at: {hail_fp}")
     mt_prs.key_cols_by().cols().write(hail_fp, overwrite=True)
     
     # Export the Hail Table to a CSV file
+    print(f"Exporting PRS scores...")
     saved_mt = hl.read_table(hail_fp)
-    print(f"PRS scores saved as: {gc_csv_fp}")
     saved_mt.export(gc_csv_fp, header=True, delimiter=',')
+    print(f"PRS scores saved as: {gc_csv_fp}")
     
     # Optional: Extract and save found variants
     if save_found_variants:
+        print("Extracting and saving found variants...")
         found_variants_table = mt_prs.filter_rows(hl.is_defined(mt_prs.prs_info)).rows()
         found_prs_info_df = found_variants_table.select(found_variants_table.prs_info).to_pandas()
         

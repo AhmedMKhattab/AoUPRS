034801bf40601f6f7eccd191222787ba61655c1a
Ahmed Khattab <akhattab@ims02.scripps.edu>
2025-05-23 14:27:08 -0700

    Bump version to 0.1.5 for PyPI release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AoUPRS',
-    version='0.1',
+    version='0.1.5',
     description='AoUPRS is a Python module for calculating Polygenic Risk Scores (PRS) specific to the All of Us study',
     author='Ahmed Khattab',
     packages=find_packages(),
